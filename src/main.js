95f8b0c231807319ff65f1997e1343b8a24c7b7e
Nilansh Sharma <80972022+nilanshsharma23@users.noreply.github.com>
2026-02-04 19:01:38 +0530

    google site verification

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "../google54e84e7a94ac5851.html";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
